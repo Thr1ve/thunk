ce87bb538392811e9928da68bb95698e7a71c58e
Thr1ve <fict1on.buhler@gmail.com>
2016-06-14 16:12:34 -0400

    update package.json and index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 'use strict';
 
-export { default as readDir } from './readDir';
-export { default as writeFile } from './writeFile';
-export { default as stringify } from './stringify';
 export { default as thunk } from './thunk';
 
 import * as utils from '.';
